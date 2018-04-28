f78b90b6f4210808a84225de10526cb0eb60e473
yeonjae <yjflore34@gmail.com>
2018-04-28 21:56:19 +0900

    #1089 PermissionUtil 추가

---
+++ app/src/main/java/smu/ac/kr/johnber/util/PermissionUtil.java
@@ -0,0 +1,29 @@
+//package smu.ac.kr.johnber.util;
+//
+//import android.app.Activity;
+//
+//import static smu.ac.kr.johnber.util.LogUtils.makeLogTag;
+//
+//public class PermissionUtil {
+//
+//    private static String TAG = makeLogTag(PermissionUtil.class);
+//    private static final int LOCATION_REQUEST_CODE=101;
+//
+//    public static boolean hasPermission(Activity activity, String permission) {
+//        // 퍼미션을 가지고 있는지 확인후, 다음 동작에서 권한 요청을 할지 말지 여부를 알려줌
+//
+//        if (permission == null) throw new IllegalArgumentException("permission is null");
+//
+//        return activity.checkSelfPermission(permission) == P
+//    }
+//
+//    public static void requestPermission(s)
+
+
+
+
+//
+//}
+//
+
+
