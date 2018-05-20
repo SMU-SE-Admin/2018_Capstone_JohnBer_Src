a84bd6e76c25df8fde412769e4524a572c224971
Yeonjae Lee <yjflore34@gmail.com>
2018-05-20 19:45:46 +0900

    Gps테스트를 위한 gpx 파일 추가

--- app/src/main/java/smu/ac/kr/johnber/run/MainActivity.java
+++ app/src/main/java/smu/ac/kr/johnber/run/MainActivity.java
@@ -27,6 +27,7 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import smu.ac.kr.johnber.BaseActivity;
@@ -206,6 +207,8 @@ public class MainActivity extends BaseActivity implements OnClickListener, OnMap
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mgoogleMap = googleMap;
+        // Map style 바꾸기 - 한국은 현행법상 적용이안된다... ㅠ
+        mgoogleMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(this,R.raw.style_json));
         if (PermissionUtil.shouldAskPermission(this, PERMISSION)) {
             //권한없는경우 default 서울로 설정 + 안내 문구
             googleMap.setMinZoomPreference(17);
