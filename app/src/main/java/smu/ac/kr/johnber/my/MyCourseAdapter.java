9148093017c1d1a3c85f09fbef39de6e7cd3dd71
JungGyeongHwan <xodlfwm96@gmail.com>
2018-09-04 19:33:37 +0900

    smu.ac.kr.my 코드 정리

--- app/src/main/java/smu/ac/kr/johnber/my/MyCourseAdapter.java
+++ app/src/main/java/smu/ac/kr/johnber/my/MyCourseAdapter.java
@@ -8,35 +8,19 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.Registry;
-import com.bumptech.glide.RequestManager;
-import com.bumptech.glide.annotation.GlideModule;
-import com.bumptech.glide.module.AppGlideModule;
 import com.bumptech.glide.request.RequestOptions;
-import com.firebase.ui.storage.images.FirebaseImageLoader;
-import com.google.firebase.storage.StorageReference;
 
-import java.io.InputStream;
-import java.util.ArrayList;
 import java.util.List;
 
-import io.realm.RealmResults;
 import smu.ac.kr.johnber.R;
-import smu.ac.kr.johnber.my.MyCourseViewHolder;
 import smu.ac.kr.johnber.run.Record;
 import smu.ac.kr.johnber.util.LocationUtil;
 import smu.ac.kr.johnber.util.RecordUtil;
 
-
-import static smu.ac.kr.johnber.util.LogUtils.LOGD;
-import static smu.ac.kr.johnber.util.LogUtils.makeLogTag;
-
 public class MyCourseAdapter extends RecyclerView.Adapter<MyCourseViewHolder>{
-  private final static String TAG = makeLogTag(MyCourseAdapter.class);
   private List<Record> data;
   private MyCourseViewHolder.itemClickListener listener;
   private Context context;
-  private RequestManager glide;
 
   public MyCourseAdapter(Context context, List<Record> data, MyCourseViewHolder.itemClickListener listener) {
     this.data = data;
@@ -62,11 +46,6 @@ public class MyCourseAdapter extends RecyclerView.Adapter<MyCourseViewHolder>{
     String time = RecordUtil.milliseconsToStringFormat(courseItem.getElapsedTime());
     holder.time.setText(time);
 
-
-//    String startPoint = LocationUtil.latlngtoStringLocation(data.get(0).getJBLocation().get(0),context);
-//    String startPointAddress[] = null;
-//    //String startPointAddress[] = startPoint.split(" ");
-
     String startPoint = LocationUtil.latlngtoStringLocation(courseItem.getJBLocation().get(0),context);
     String startPointAddress[] = startPoint.split(" ");
 
@@ -76,7 +55,6 @@ public class MyCourseAdapter extends RecyclerView.Adapter<MyCourseViewHolder>{
       startPoint = startPointAddress[2] + " " + startPointAddress[3];
     }
     holder.startPoint.setText(startPoint);
-
     holder.CAL.setVisibility(View.VISIBLE);
     holder.CAL.setText("CAL");
     holder.KM.setText("KM");
@@ -84,18 +62,16 @@ public class MyCourseAdapter extends RecyclerView.Adapter<MyCourseViewHolder>{
 
     //썸네일 다운로드
     Uri uri = Uri.parse(data.get(position).getImgUrl());
-//    LOGD(TAG,"DownloadIMG_path : "+ data.get(position).getImgUrl());
     if (uri != null) {
       //placeholder : 이미지 로딩중 미리 보여지는 이미지
-    Glide.with(context)
-            .load(uri)
-            .apply(new RequestOptions().centerCrop()
-                            .placeholder(R.drawable.ic_glide_placeholder)
-                      .error(R.drawable.ic_glide_placeholder)
-            )
-            .thumbnail(0.1f)
-            .into(holder.thumnail);
-    //listener
+      Glide.with(context)
+              .load(uri)
+              .apply(new RequestOptions().centerCrop()
+                              .placeholder(R.drawable.ic_glide_placeholder)
+                        .error(R.drawable.ic_glide_placeholder)
+              )
+              .thumbnail(0.1f)
+              .into(holder.thumnail);
     }
   }
 
@@ -103,7 +79,6 @@ public class MyCourseAdapter extends RecyclerView.Adapter<MyCourseViewHolder>{
   public int getItemCount() {
     return data.size();
   }
-
 
   }
 
